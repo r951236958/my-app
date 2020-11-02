ee969be85a89416ac7183a16e4d3393daa0b5584
nick <r951236958@gmail.com>
2020-11-02 17:33:35 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Toast from 'react-bootstrap/Toast'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ExampleToast = ({ children }) => {
+    const [show, toggleShow] = useState(true)
+
+    return (
+        <>
+            {!show && (
+                <Button onClick={() => toggleShow(true)}>Show Toast</Button>
+            )}
+            <Toast show={show} onClose={() => toggleShow(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">React-Bootstrap</strong>
+                </Toast.Header>
+                <Toast.Body>{children}</Toast.Body>
+            </Toast>
+        </>
+    )
 }
 
-export default App;
+function AlertDismissible() {
+    const [show, setShow] = useState(true);
+  
+    return (
+      <>
+        <Alert show={show} variant="success">
+          <Alert.Heading>How's it going?!</Alert.Heading>
+          <p>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+            lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+            fermentum.
+          </p>
+          <hr />
+          <div className="d-flex justify-content-end">
+            <Button onClick={() => setShow(false)} variant="outline-success">
+              Close me y'all!
+            </Button>
+          </div>
+        </Alert>
+  
+        {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
+      </>
+    );
+  }
+
+const App = () => (
+    <Container className="p-3">
+        <Jumbotron>
+            <h1 className="header">Welcome To React-Bootstrap</h1>
+            <ExampleToast>
+                We now have Toasts
+                <span role="img" aria-label="tada">
+                    🎉
+                </span>
+            </ExampleToast>
+            <AlertDismissible />
+        </Jumbotron>
+    </Container>
+)
+
+export default App
